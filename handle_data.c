8e7d708726f0defe05a3f50f1d417ff0cdd14b65
Matilde Silva <114365956+whtifigo@users.noreply.github.com>
2025-03-11 12:36:45 +0000

    "pipex going... 🚶‍➡️"

--- handle_data.c
+++ handle_data.c
@@ -1,9 +1,60 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
+#include "../pipex.h"
 
 exit
-free
+
+void	free_rest(char *rest)
+{
+	size_t	i;
+
+	i = 0;
+	while (rest[i])
+	{
+		free(rest[i])
+		i++;
+	}
+	free(rest);
+}
+
 file (opener in and out)
-env
-path
+
+char	**get_env(char **envp)
+{
+	int	i;
+
+	i = 0;
+	while (envp[i] && ft_strnstr(envp[i], "PATH=", 5) != envp[i]) 
+	       i++;
+
+	if (!envp[i])
+		return (NULL);
+
+	return (ft_split(envp[i] + 5, ":"));
+}
+
+char	*get_path(char *cmd, char **envp)
+{
+	int	i;
+	char	*pathing;
+	char	*path_full;
+	char	**pathen;
+
+	i = 0;
+	pathen = get_env(envp)
+	if (!pathen)
+		return NULL;
+	while (pathen[i])
+	{
+		pathing = ft_strjoin(pathen[i], "/");
+		path_full = ft_strjoin(pathing, cmd);
+		free(pathing);
+		if (access(path_full, F_OK) == 0)
+			return (path_full);
+		free(path_full);
+		i++;
+	}
+	i = -1;
+	while (pathen[++i])
+		free(pathen[i]);
+	free(pathen);
+	return (NULL);
+}
---
+++ pipex.h
@@ -0,0 +1,9 @@
+#ifndef PIPEX_H
+# define PIPEX_H
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "../libft/libft.h"
+
+#endif
